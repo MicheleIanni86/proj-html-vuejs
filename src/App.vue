4d2993165676ac0f82a031d3486b5f7fbc661fc4
MicheleIanni86 <micheleianni86@gmail.com>
2024-02-19 13:44:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from './store';
+
+export default {
+  data() {
+    return { store };
+
+  },
+
+
+
+
+};
+</script>
+
+<template>
+  <h1>Ciao!!!</h1>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
